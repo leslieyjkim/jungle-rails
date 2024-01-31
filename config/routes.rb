8acf9a13ddda95ba89dad73a6030be50f7c3fd5f
yoonjookim <yjblossom@gmail.com>
2024-01-30 21:50:46 -0500

    feature/user-auth: generate user controller by using : bin/rails generate controller user

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
   # get to /about, will call index method at about controller
   get '/about' => 'about#index'
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
