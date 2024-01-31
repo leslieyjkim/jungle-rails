12ae3d6f82b30d6e391e61c6580ca2a8fa0a1e2b
yoonjookim <yjblossom@gmail.com>
2024-01-30 19:04:17 -0500

    adding about us page at header/footer both, set routes, create about view

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  # get to /about, will call index method at about controller
+  get '/about' => 'about#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
